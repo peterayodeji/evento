3299d97b80c6dfb86337a57627efcd810b56d7e9
Peter Ayodeji <pb.ayodeji@gmail.com>
2026-01-23 14:15:02 +0100

    build event page intro section

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,5 +1,68 @@
-function EventPage() {
-  return <main>Event Page</main>;
+import H1 from '@/components/h1';
+import Image from 'next/image';
+
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+async function EventPage({ params }: EventPageProps) {
+  const slug = params.slug;
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`,
+  );
+  const event = await response.json();
+
+  return (
+    <main>
+      <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
+        <Image
+          src={event.imageUrl}
+          className="object-cover z-0 blur-3xl"
+          alt="Event background image"
+          fill
+          quality={50}
+          sizes="(max-width: 1280px) 100vw, 1280px"
+          priority
+        />
+
+        <div className="z-1 flex flex-col gap-6 lg:gap-16 lg:flex-row relative">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+            className="rounded-xl border-2 border-white/50 object-cover"
+          />
+
+          <div className="flex flex-col">
+            <p className="text-white/75">
+              {new Date(event.date).toLocaleDateString('en-US', {
+                weekday: 'long',
+                month: 'long',
+                day: 'numeric',
+              })}
+            </p>
+
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
+
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+
+            <button className="bg-white/20 text-lg capitalize bg-blur mt-5 lg:mt-auto w-[95vw] rounded-md border-white/10 border-2 sm:w-full py-2 state-effects">
+              Get tickets
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <div></div>
+    </main>
+  );
 }
 
 export default EventPage;
