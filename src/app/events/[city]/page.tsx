3305f15da2430a8ae466c4574324f369637f83f0
Peter Ayodeji <pb.ayodeji@gmail.com>
2026-01-26 16:45:10 +0100

    generate dynamic metadata each different page

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,25 +1,37 @@
 import { Suspense } from 'react';
-import { EventoEvent } from '@/lib/types';
+import { capitalize } from '@/lib/utils';
+import { Metadata } from 'next';
 
 import H1 from '@/components/h1';
 import EventsList from '@/components/events-list';
 import Loading from './loading';
 
-type EventsPageProps = {
+type Props = {
   params: {
     city: string;
   };
 };
 
-async function EventsPage({ params }: EventsPageProps) {
+export function generateMetadata({ params }: Props): Metadata {
+  const city = params.city;
+
+  return {
+    title: city === 'all' ? 'All Events' : `Events in ${capitalize(city)}`,
+  };
+}
+
+// export const metadata: Metadata = {
+//   title: 'Events in Austin',
+// };
+
+async function EventsPage({ params }: Props) {
   const city = params.city;
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">
         {city === 'all' && 'All Events'}
-        {city !== 'all' &&
-          `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
+        {city !== 'all' && `Events in ${capitalize(city)}`}
       </H1>
 
       <Suspense fallback={<Loading />}>
