0dd370ecf6237b9157011311e8298a1d76b15353
Peter Ayodeji <pb.ayodeji@gmail.com>
2026-01-26 15:03:21 +0100

    replace loading text with skeleton component

--- src/app/event/loading.tsx
+++ src/app/event/loading.tsx
@@ -1,5 +1,13 @@
+import Skeleton from '@/components/skeleton';
+
 function Loading() {
-  return <div>Loading city...</div>;
+  return (
+    <div className="flex flex-col items-center gap-y-4 pt-28">
+      <Skeleton className="h-4 w-[550px]" />
+      <Skeleton className="h-4 w-[480px]" />
+      <Skeleton className="h-4 w-[430px]" />
+    </div>
+  );
 }
 
 export default Loading;
